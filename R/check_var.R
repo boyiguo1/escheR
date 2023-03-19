a6a8bb98605c4080f8761ed2dbfc49bf88dad619
Boyi Guo <boyi.guo.work@gmail.com>
2023-03-18 22:34:14 -0400

    Add stephanie to description

---
+++ R/check_var.R
@@ -0,0 +1,23 @@
+# TODO: to delete
+check_var <- function(p, var){
+  browser()
+  in_colData <- var %in% colnames(p$data)
+  index_gene_id <- which(rownames(p$spe) == var)
+  # index_gene_name <- which(rowData(p$spe)$gene_name == var)
+
+  if(in_colData) return(p)
+
+  if(length(index_gene_id)!=0){
+    p$data[[var]] <- p$spe[[var]]
+
+    return(p)
+  }
+
+  # No matches
+  # if(!any(var %in% colnames(p$data),
+  #         var %in% rownames(p$spe),
+  #         var %in% rowData(p$spe)$gene_name
+  # )
+  # )
+  stop(paste0("The variable ", var, " are not in the spe object, as colData or genes."))
+}
