df08739b9c333fe5a85b6a5a1734252cb4dd9267
Boyi Guo <boyi.guo.work@gmail.com>
2023-03-23 22:35:04 -0400

    add documentation

--- R/add_ground.R
+++ R/add_ground.R
@@ -1,5 +1,28 @@
+#' Adding border to highlight the ground in the spatial map
+#'
+#' @param p a spatial map created by \code{make_escheR()}, with or without other layers of aesthetics.
+#' @param var A character(1) with the name of the colData(spe) column that has the values to be used as the background.
+#' @param stroke A numeric(1) specifying the thickness of the border.
+#' @param point_size A numeric(1) specifying the size of the spot in the ggplot. Defaults to 2.
+#' @param ... Reserved for future arguments.
+#'
+#' @return an ggplot object.
+#'
 #' @export
-add_ground.SpatialExperiment <- function(
+#' @examples
+#' #'\dontrun{
+#'library(spatialLIBD)
+#'
+#'if (!exists("spe"))
+#'   spe <- fetch_data("spatialDLPFC_Visium")
+#'
+#'# Subset one sample
+#'spe <- spe[, spe$sample_id == "Br8667_mid"]
+#'
+#'make_escheR(spe) |>
+#' add_ground(var = "BayesSpace_harmony_09")
+#'}
+add_ground <- function(
     p,
     var,
     stroke = 0.5,
@@ -13,20 +36,16 @@ add_ground.SpatialExperiment <- function(
   if(!var %in% colnames(p$data))
     stop(paste0("Please add the variable ", var, " to colData(spe)."))
 
-  # p <- check_var(p, var)
-
-  # browser()
   if(is.numeric(p$data[,var])) {
     p <- suppressMessages({p + scale_color_continuous()})
   } else{
     p <- suppressMessages({p + scale_color_discrete()})
   }
 
-
   p +
     geom_point(
       aes(color = !!sym(var),
-    ),
+      ),
       shape = 21,
       fill = "transparent",
       size = point_size,
